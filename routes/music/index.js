f0f16941c419232c4da4aa1d6ed0e456f3f5c6e9
Eling <evanchen486@outlook.com>
2022-01-21 15:40:14 +0800

    fixed bugs - 20220121

--- routes/music/index.js
+++ routes/music/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { sendJSON } = require('../utils/index')
 
 router.get('/', function (req, res, next) {
-    sendJSON(res, 0, '音乐平台API正常运行中', {available: ['ncm', 'qq']});
+    return sendJSON(res, 0, '音乐平台API正常运行中', {available: ['ncm', 'qq']});
 });
 
 module.exports = router
